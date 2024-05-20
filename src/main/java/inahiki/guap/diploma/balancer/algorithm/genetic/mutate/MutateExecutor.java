559ee9d593ac6b9a248eb5366d59e613a7c0d075
Inahiki <skyres.leibuk@gmail.com>
2024-05-20 17:27:24 +0300

    load

--- src/main/java/inahiki/guap/diploma/balancer/algorithm/genetic/mutate/MutateExecutor.java
+++ src/main/java/inahiki/guap/diploma/balancer/algorithm/genetic/mutate/MutateExecutor.java
@@ -2,8 +2,6 @@ package inahiki.guap.diploma.balancer.algorithm.genetic.mutate;
 
 import inahiki.guap.diploma.segment.Segment;
 import inahiki.guap.diploma.segment.SegmentDistribution;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Random;
@@ -14,11 +12,11 @@ public interface MutateExecutor {
 
     /**
      * Ищет случайный сегмент, который не соответствует
-     * @param segments Список сегментов
-     * @param parentSegment Парный сегмент
-     * @return Возвращает случайный неповторяемый сегмент
+     * @param segments          Список сегментов
+     * @param parentSegment     Парный сегмент
+     * @return                  Возвращает случайный неповторяемый сегмент
      */
-    default @NotNull Segment getRandomSegment(@NotNull List<Segment> segments, @Nullable Segment parentSegment, @NotNull Random random) {
+    default Segment getRandomSegment(List<Segment> segments, Segment parentSegment, Random random) {
         Segment randomSegment;
         do {
             int randomId = random.nextInt(0, segments.size());
